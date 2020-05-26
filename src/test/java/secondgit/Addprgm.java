2c707ae802b2e1a00c9aefde61bd517cb8214fb8
vidhya12278 <evidhya60@gmail.com>
2020-05-26 07:10:38 +0530

    this is my project

--- src/test/java/secondgit/Addprgm.java
+++ src/test/java/secondgit/Addprgm.java
@@ -8,5 +8,13 @@ public class Addprgm {
 	int c;
 	c=a+b;
 	System.out.println("add value:  " + c);
+	
+	int a1=10;
+	int b1=19;
+	int c1;
+	c1=a1+b1;
+	System.out.println("sub value:  " + c1);
+	
+	
 }
 }
