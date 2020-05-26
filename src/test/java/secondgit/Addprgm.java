c021e4ff40133519157d36dc010e8f2d90e69dfc
vidhya12278 <evidhya60@gmail.com>
2020-05-26 07:03:17 +0530

    design

---
+++ src/test/java/secondgit/Addprgm.java
@@ -0,0 +1,12 @@
+package secondgit;
+
+public class Addprgm {
+	public static void main(String[] args) {
+		
+	int a=10;
+	int b=19;
+	int c;
+	c=a+b;
+	System.out.println("add value:  " + c);
+}
+}
